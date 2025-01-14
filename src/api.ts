32ba463d76a4bb32fa760bd67f8307daf2b0ea54
Zefir Kirilov <contact@zefir.pro>
2025-01-15 01:44:28 +0200

    fix copyright notice

--- src/api.ts
+++ src/api.ts
@@ -3,15 +3,15 @@
  *
  * This file is part of eatc-airlines.
  *
- * eatc-airlines is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * eatc-airlines is free software: you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  *
  * eatc-airlines is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along with eatc-airlines.
+ * You should have received a copy of the GNU General Public License along with eatc-airlines.
  * If not, see <https://www.gnu.org/licenses/>.
  */
 export class LatLon {
