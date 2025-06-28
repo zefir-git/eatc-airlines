d52c559fe056806d8a84025121696779c6156074
Zefir Kirilov <contact@zefir.pro>
2025-06-28 18:39:29 +0300

    improve console logging of API error

--- src/cli.ts
+++ src/cli.ts
@@ -111,8 +111,9 @@ async function get(icao: string, t: Date, ua?: string, cf?: string, session?: st
             res.status === 403
             && res.headers.get("content-type") !== "application/json"
         )
-            console.log("\r\n\x1b[0;33mYou are likely facing a Cloudflare challenge. Please see \x1b[1;33m\x1b]8;;https://github.com/zefir-git/eatc-airlines/pull/52#issue-3145483139\x1b\\#52\x1b]8;;\x1b\\\x1b[0;33m on how to resolve it.\x1b[0m");
-        throw new Error(`API returned ${res.status} (${res.statusText}) for ${url}`);
+            console.warn("\r\n\x1b[0;33mYou are likely facing a Cloudflare challenge. Please see \x1b[1;33m\x1b]8;;https://github.com/zefir-git/eatc-airlines/pull/52#issue-3145483139\x1b\\#52\x1b]8;;\x1b\\\x1b[0;33m on how to resolve it.\x1b[0m");
+        console.error(new Error(`API returned ${res.status} (${res.statusText}) for ${url}`));
+        process.exit(1);
     }
     const body = await res.text();
     try {
